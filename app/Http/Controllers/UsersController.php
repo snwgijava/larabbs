3ddca6d4565ab4db34072908e69155f202a19850
yangjian <809127232@qq.com>
2018-01-14 13:55:53 +0000

    添加上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
@@ -20,8 +21,18 @@ class UsersController extends Controller
     }
 
     //UserRequest是表单请求验证（FormRequest） 是 Laravel 框架提供的用户表单数据验证方案，此方案相比手工调用 validator 来说，能处理更为复杂的验证逻辑，更加适用于大型程序
-    public function update(UserRequest $request,User $user){
-        $user->update($request->all());
+    public function update(UserRequest $request,User $user,ImageUploadHandler $uploader){
+
+        $data = $request->all();
+
+        if($request->avatar){
+            $result = $uploader->save($request->avatar,'avatars',$user->id,362);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功!');
     }
 }
