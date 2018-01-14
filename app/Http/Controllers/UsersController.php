a5c329d7079fbe3793ba625d5492f483a9b3ac70
yangjian <809127232@qq.com>
2018-01-14 14:06:42 +0000

    授权用户才能修改用户信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,18 +11,28 @@ class UsersController extends Controller
 {
     //用户信息相关
 
+    public function __construct()
+    {
+        //except是除了指定动作以外都需要登录才能操作
+        $this->middleware('auth',['except' => ['show']]);
+    }
+
 
     public function show(User $user){
         return view('users.show',compact('user'));
     }
 
     public function edit(User $user){
+        //authorize 方法接收两个参数，第一个为授权策略的名称，第二个为进行授权验证的数据
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     //UserRequest是表单请求验证（FormRequest） 是 Laravel 框架提供的用户表单数据验证方案，此方案相比手工调用 validator 来说，能处理更为复杂的验证逻辑，更加适用于大型程序
     public function update(UserRequest $request,User $user,ImageUploadHandler $uploader){
 
+        //authorize 方法接收两个参数，第一个为授权策略的名称，第二个为进行授权验证的数据
+        $this->authorize('update',$user);
         $data = $request->all();
 
         if($request->avatar){
