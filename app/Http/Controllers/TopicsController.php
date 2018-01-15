131365570e82ce2bd8a91a530d884a0b5746b576
yangjian <809127232@qq.com>
2018-01-15 13:23:53 +0000

    修改帖子主页样式

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -16,7 +16,8 @@ class TopicsController extends Controller
 
 	public function index()
 	{
-		$topics = Topic::paginate();
+	    //with方法是预加载功能，解决N+1的问题，user和category是需要用到的关联属性，并做了缓存
+		$topics = Topic::with('user','category')->paginate();
 		return view('topics.index', compact('topics'));
 	}
 
