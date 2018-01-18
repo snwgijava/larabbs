8b6648cc8c16d50959b42e8e4f271ec205d3ad46
yangjian <809127232@qq.com>
2018-01-18 14:49:55 +0000

    整合laravel权限包

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    use HasRoles;   //导入第三方权限
+
     use Notifiable{
         notify as protected laravelNotify;
     }
