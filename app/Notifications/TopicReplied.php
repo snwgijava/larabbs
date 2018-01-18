b85f6318bcfd2c6f15004225bdd503b2370cdbc0
yangjian <809127232@qq.com>
2018-01-18 13:39:18 +0000

    在队列支持的电子邮件中通知用户

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -8,7 +8,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class TopicReplied extends Notification
+class TopicReplied extends Notification implements ShouldQueue
 {
     use Queueable;
 
@@ -34,7 +34,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database','mail'];
     }
 
     /**
@@ -72,5 +72,11 @@ class TopicReplied extends Notification
         return [
             //
         ];
+    }
+
+    public function toMail($notifiable){
+        $url = $this->reply->topic->link(['$reply' . $this->reply->id]);
+
+        return (new MailMessage)->line('你的话题有新的回复！')->action('查看回复',$url);
     }
 }
